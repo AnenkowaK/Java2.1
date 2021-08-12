5227ac0ec9f0e053c87f727a93ae896de4fec077
Ekaterina Smetanina <AnenkowaK@mail.ru>
2021-08-12 17:40:19 +0300

    initial commit

---
+++ src/Name.java
@@ -0,0 +1,10 @@
+public class Name {
+    public static void main(String[] args) {
+
+        float priceTiket = 19;
+        float priceMile = 20;
+        int bonus = (int) (priceTiket/priceMile);
+
+        System.out.println(bonus);
+    }
+}
